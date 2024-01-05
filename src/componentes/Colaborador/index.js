ada8cbea5e79be613dea004c6173f854f224add4
Josan Behrends <josanbehrends@gmail.com>
2024-01-05 15:22:14 -0300

    Finalização do projeto

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,19 @@
 import { IoClose } from "react-icons/io5";
+import { CiHeart } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
+
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id);
+    }
+    
+    const propsfavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
     return (<div className="colaborador">
         <IoClose 
             size={20} 
@@ -14,6 +26,12 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className="favoritar">
+                {colaborador.favorito 
+                    ? <CiHeart {...propsfavorito}/> 
+                    : <FaHeart {...propsfavorito} color='ff0000'/>
+                }
+            </div>
         </div>
     </div>)
 }
